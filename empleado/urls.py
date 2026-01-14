5f464bd3e3f87c3f4e14d30edaa6a99102f36013
XimeTapia <tavx080415mmcplma3@soycecytem.mx>
2026-01-13 21:12:20 -0600

    Update urls.py

--- empleado/urls.py
+++ empleado/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, re_path, include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -7,6 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     # URLs de las apps existentes
-    re_path('', include('applications.departamento.urls')),
-    re_path('', include('applications.home.urls')),
+    path('departamento/', include('applications.departamento.urls')),  # Prefijo departamento
+    path('', include('applications.home.urls')),  # Home en la raíz
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
