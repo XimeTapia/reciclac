b3addf9eb22fde97ab5f083aae253a4691ec7d72
XimeTapia <tavx080415mmcplma3@soycecytem.mx>
2026-01-13 19:51:49 -0600

    Update base.py

--- empleado/settings/base.py
+++ empleado/settings/base.py
@@ -57,3 +57,10 @@ USE_I18N = True
 USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
