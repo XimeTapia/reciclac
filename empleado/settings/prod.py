e001709f3c275d477548320c8cc042b53866a677
XimeTapia <tavx080415mmcplma3@soycecytem.mx>
2026-01-13 19:38:28 -0600

    Update prod.py

--- empleado/settings/prod.py
+++ empleado/settings/prod.py
@@ -1,8 +1,18 @@
 from .base import *
 
+DEBUG = False
+
 ROOT_URLCONF = 'empleado.urls'
 
 ALLOWED_HOSTS = [
     'reciclac-1.onrender.com',
     '.onrender.com',
 ]
+
+# Static files
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
