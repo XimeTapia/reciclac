a511f3e4252a3d6ff1f6cce85c713030294fdd3e
XimeTapia <tavx080415mmcplma3@soycecytem.mx>
2026-01-13 21:50:58 -0600

    Update models.py

--- applications/departamento/models.py
+++ applications/departamento/models.py
@@ -1,16 +1,15 @@
 from django.db import models
 
-# Create your models here.
 class Departamento(models.Model):
     nombre = models.CharField('Nombre', max_length=50, editable=True)
     subnombre = models.CharField('Nombre_corto', max_length=50, unique=True)
-    activo= models.BooleanField('Anulado', default='false')
- 
+    activo = models.BooleanField('Anulado', default=False)
+
     class Meta:
-        verbose_name='Mi departamento'
-        verbose_name_plural='Areas de la empresa'
-        ordering=['-nombre']
-        unique_together=('nombre','subnombre')
-    
+        verbose_name = 'Mi departamento'
+        verbose_name_plural = 'Areas de la empresa'
+        ordering = ['-nombre']
+        unique_together = ('nombre', 'subnombre')
+
     def __str__(self):
-        return str(self.id)+ '-'+ self.nombre +'-' + self.subnombre
+        return f"{self.id}-{self.nombre}-{self.subnombre}"
